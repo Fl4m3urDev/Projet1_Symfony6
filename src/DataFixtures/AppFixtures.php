ab526f72f03d9e2c0c28bcaf0b5b72e88bef707b
Fl4m3urDev <VoxFlameur22@gmail.com>
2024-09-09 15:23:54 +0200

    add Entity User + Firewall and Security + Hash Password + Form login and register + add controller 'Security' + templates + EntityListener and more...

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,8 +6,10 @@ use Faker\Factory;
 use Faker\Generator;
 use App\Entity\Recipe;
 use App\Entity\Ingredient;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -26,17 +28,17 @@ class AppFixtures extends Fixture
     {
         // Ingredients
         $ingredients = [];
-        for ($i=0; $i<50; $i++) { 
+        for ($i = 0; $i < 50; $i++) {
             $ingredient = new Ingredient();
             $ingredient->setName($this->faker->word())
-            ->setPrice(mt_rand(0, 100));
+                ->setPrice(mt_rand(0, 100));
 
             $ingredients[] = $ingredient;
             $manager->persist($ingredient);
         }
 
         // Recipes
-        for ($j=0; $j<25; $j++) { 
+        for ($j = 0; $j < 25; $j++) {
             $recipe = new Recipe();
             $recipe->setName($this->faker->word())
                 ->setTime(mt_rand(0, 1) == 1 ? mt_rand(1, 1440) : null)
@@ -46,10 +48,22 @@ class AppFixtures extends Fixture
                 ->setPrice(mt_rand(0, 1) == 1 ? mt_rand(1, 1000) : null)
                 ->setIsFavorite(mt_rand(0, 1) == 1 ? true : false);
 
-                for ($k=0; $k<mt_rand(5, 15); $k++) { 
-                    $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
-                }
-                $manager->persist($recipe);
+            for ($k = 0; $k < mt_rand(5, 15); $k++) {
+                $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
+            }
+            $manager->persist($recipe);
+        }
+
+        // Users
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('password');
+
+            $manager->persist($user);
         }
 
         $manager->flush();
